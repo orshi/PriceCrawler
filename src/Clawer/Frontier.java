9de4ffef42816bd3f09f94c9568d6018216df0d9
ora xie <oxie@localhost.localdomain>
2015-08-26 07:55:55 +0800

    remove unused files

--- src/Clawer/Frontier.java
+++
@@ -1,7 +0,0 @@
-package Clawer;
-
-public interface Frontier {
-	public CrawlUrl GetNext()throws Exception;
-	public boolean PutUrl(CrawlUrl url)throws Exception;
-
-}
--- src/Clawer/LinkFilter.java
+++
@@ -1,5 +0,0 @@
-package Clawer;
-
-public interface LinkFilter{
-	public boolean accept(String url);
-}
